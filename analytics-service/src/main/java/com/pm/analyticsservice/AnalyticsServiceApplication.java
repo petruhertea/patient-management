26748f2075e91647b5788014e4e0557156f43c80
Petru <petre.hertea@gmail.com>
2026-03-24 15:49:40 +0200

    - add Kafka Producer in patient-service - add Kafka Consumer in analytics-service - add API gateway that routes to patient-service

--- analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
@@ -1,14 +1,13 @@
-package com.pm.patientservice;
+package com.pm.analyticsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-public class PatientServiceApplication {
+public class AnalyticsServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PatientServiceApplication.class, args);
+        SpringApplication.run(AnalyticsServiceApplication.class, args);
     }
 
 }
--- api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/pm/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
-package com.pm.patientservice;
+package com.pm.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-public class PatientServiceApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PatientServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
